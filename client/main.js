b40972b6df9da86beaf1f213e4d30cb4d71db581
justbraf <dwaynecorey+github@gmail.com>
2021-03-04 15:45:39 -0400

    implemented add & sub;

--- client/main.js
+++ client/main.js
@@ -46,8 +46,34 @@ Template.addNum.helpers({
 Template.addNum.events({
   'click #js-adding'(event, instance) {
     console.log("sum button clicked");
-    instance.num1.set(parseInt(document.querySelector("#n1").value));
-    instance.num2.set(parseInt(document.querySelector("#n2").value));
+    instance.num1.set(parseInt(document.querySelector("#n1a").value));
+    instance.num2.set(parseInt(document.querySelector("#n2a").value));
+  },
+});
+
+Template.subNum.onCreated(function subNumOnCreated() {
+  // counter starts at 0
+  this.num1 = new ReactiveVar(0);
+  this.num2 = new ReactiveVar(0);
+});
+
+Template.subNum.helpers({
+  num1(){
+    return Template.instance().num1.get();
+  },
+  num2(){
+    return Template.instance().num2.get();
+  },
+  ans(){
+    return Template.instance().num1.get() - Template.instance().num2.get();
+  },
+});
+
+Template.subNum.events({
+  'click #js-subing'(event, instance) {
+    console.log("diff button clicked");
+    instance.num1.set(parseInt(document.querySelector("#n1s").value));
+    instance.num2.set(parseInt(document.querySelector("#n2s").value));
   },
 });
 
