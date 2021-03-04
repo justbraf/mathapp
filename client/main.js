ed4c9d80c6e4227aa4c152845fb5d6a0d244df9a
justbraf <dwaynecorey+github@gmail.com>
2021-03-04 15:04:56 -0400

    learn templates

--- client/main.js
+++ client/main.js
@@ -19,4 +19,60 @@ Template.hello.events({
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
   },
+  'click h3'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 2);
+  },
+});
+
+Template.addNum.onCreated(function addNumOnCreated() {
+  // counter starts at 0
+  this.num1 = new ReactiveVar(0);
+  this.num2 = new ReactiveVar(0);
+});
+
+Template.addNum.helpers({
+  num1(){
+    return Template.instance().num1.get();
+  },
+  num2(){
+    return Template.instance().num2.get();
+  },
+  ans(){
+    return Template.instance().num1.get() + Template.instance().num2.get();
+  },
+});
+
+Template.addNum.events({
+  'click #js-adding'(event, instance) {
+    console.log("sum button clicked");
+    instance.num1.set(parseInt(document.querySelector("#n1").value));
+    instance.num2.set(parseInt(document.querySelector("#n2").value));
+  },
+});
+
+let gName = "Brad";
+
+Template.allData.helpers({
+  guestName(){
+    return gName;
+  },
+  skill(){
+    return "coding"
+  },
+  mySkill(){
+    if (gName == "Brad"){
+      return true;
+    }
+    return false;
+  }
+});
+
+Template.allData.events({
+  'mouseout h3'(event, instance){
+    console.log("Welcome");
+  },
+  'mouseenter h6'(event, instance){
+    console.log("Header 6 touched");
+  }
 });
